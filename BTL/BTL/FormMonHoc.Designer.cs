7a9637891358ddc33592acf1c26d08dadfd0213b
pham ha <phamtuanha21092003@gmail.com>
2023-03-28 21:45:15 +0700

    test commit

--- BTL/BTL/FormMonHoc.Designer.cs
+++ BTL/BTL/FormMonHoc.Designer.cs
@@ -55,7 +55,7 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(12, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 16);
+            this.label2.Size = new System.Drawing.Size(117, 16);
             this.label2.TabIndex = 3;
             this.label2.Text = "Thông tin môn học";
             // 
